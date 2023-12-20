414bbc6470477ca18ff61e712726febffbf3e3b9
gksma <dhfz1794@naver.com>
2023-12-21 01:07:15 +0900

    first commit

---
+++ build/CMakeFiles/CMAKE_TEST_PROJECT_MAIN.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMAKE_TEST_PROJECT_MAIN"
+  "CMAKE_TEST_PROJECT_MAIN.pdb"
+  "CMakeFiles/CMAKE_TEST_PROJECT_MAIN.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMAKE_TEST_PROJECT_MAIN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
